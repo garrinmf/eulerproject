db7aea75e8c2de2594989c4e7db47bf5eb611ae5
Mark Garringer <mark@thornberryconsulting.com>
2010-09-17 14:21:15 -0400

    added my edgecase koans to this repo for storage and sharing between computers

---
+++ koans/triangle.rb
@@ -0,0 +1,50 @@
+
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def triangle(a, b, c)
+  if a == 0 && b == 0 && c == 0
+    raise TriangleError , "Invalid Triangle"
+  end
+
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, "Invalid Triangle"
+  end
+
+  if a == c && a == b
+    :equilateral
+  elsif((a == c && a != b))
+    if(a + c <= b)
+      raise TriangleError, "Invalid triangle"
+    end
+    :isosceles
+  elsif( a == b && a != c )
+    if(a + b <= c)
+      raise TriangleError, "Invalid triangle"
+    end
+    :isosceles
+  elsif( b == c && b != a)
+    if(b + c <= a)
+      raise TriangleError, "Invalid triangle"
+    end
+    :isosceles
+  else
+    :scalene
+  end  
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
